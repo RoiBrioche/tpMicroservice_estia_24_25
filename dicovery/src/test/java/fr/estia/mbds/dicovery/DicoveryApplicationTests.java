53e6382f5afde5f75564332d4a4a7b74c0ec665c
brieuc.mandin <brieuc.mandin@etu.estia.fr>
2025-02-06 15:40:15 +0100

    discovery service

---
+++ dicovery/src/test/java/fr/estia/mbds/dicovery/DicoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.estia.mbds.dicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class DicoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
